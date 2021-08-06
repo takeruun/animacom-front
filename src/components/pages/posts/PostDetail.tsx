568a54931738de64c88d0221c1a595999f1a81a6
takeru.hashimoto <hstake25@gmail.com>
2021-08-07 01:47:40 +0900

    こめんと module

--- src/components/pages/posts/PostDetail.tsx
+++ src/components/pages/posts/PostDetail.tsx
@@ -1,11 +1,14 @@
 import { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from 're-ducks/store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { createCommentSocketAPI } from 'api/Endpoint';
+import { AppDispatch, RootState } from 're-ducks/store/store';
 import { PostType } from 're-ducks/post/types';
 import { fetchPost } from 'modules/postModule';
+import { fetchComments } from 'modules/commentModulet';
 import { makeStyles } from '@material-ui/core/styles';
 import parse from 'html-react-parser';
+import Button from '@material-ui/core/Button';
 import { ImageSwiper, SizeTable } from './index';
 
 const useStyles = makeStyles((theme) => ({
@@ -46,19 +49,38 @@ const returnCodeToBr = (text: string) => {
   return parse(text.replace(/\r?\n/g, '<br/>'));
 };
 
+type Socket = ActionCable.Channel & {
+  create: (userId: string, body: string) => void;
+  received: (data: any) => void;
+  disconnected: () => void;
+}
+
 const PostDetail: FC = () => {
   const classes = useStyles();
   const dispatch: AppDispatch = useDispatch();
+  const commentModule = useSelector((state: RootState) => state.comment);
+
   const { id }: { id: string } = useParams();
   const [post, setPost] = useState<PostType>();
+  const [socket, setSocket] = useState<Socket>();
+
+  console.log(commentModule.comment);
 
   useEffect(() => {
     const execApi = async (postId: string) => {
       const data = await dispatch(fetchPost(postId)).unwrap();
       setPost(data);
+      setSocket(dispatch(createCommentSocketAPI(id)));
+      dispatch(fetchComments(postId));
     };
     execApi(id);
   }, [dispatch, id]);
+
+  useEffect(() => () => socket?.disconnected(), [socket]);
+
+  const createComment = (userId: string, body: string) => {
+    socket?.create(userId, body);
+  };
 
   return (
     <section className="c-section-wrapin">
