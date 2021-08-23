427f2b6228ba4def412d8e231f5904f0bd373108
hashimoto <t-hashimoto@anysense.co.jp>
2021-08-23 21:16:35 +0900

    post edit 編集 test

--- src/test/posts/PostEdit.test.tsx
+++ src/test/posts/PostEdit.test.tsx
@@ -2,7 +2,9 @@ import { Provider, useSelector } from 'react-redux';
 import { render, fireEvent, screen } from '@testing-library/react';
 import { store } from 're-ducks/store/store';
 import { PostEdit } from 'components/pages/posts';
-import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event'
+import { PostType } from 'modules/postModule';
+import axios from 'axios';
 
 const mockDispatch = jest.fn();
 jest.mock('axios');
@@ -18,82 +20,180 @@ jest.mock('react-redux', () => ({
 describe('renders post edit', () => {
   const useSelectorMock = useSelector as jest.Mock;
 
-  beforeEach(() => {
-    useSelectorMock.mockImplementation((selector: any) => selector(mockStore));
-  });
-
-  afterEach(() => {
-    jest.restoreAllMocks();
-  });
-
-  const mockStore = {
-    post: {
-      loading: false,
-    },
-    category: {
-      categories: [
-        { id: '1', name: 'category' },
-      ],
-    },
-  };
-  const renderComponent = () => render(
-    <Provider store={store}>
-      <PostEdit />
-    </Provider>
-  );
-
-  it('タイトル入力できる', async () => {
-    renderComponent();
-
-    const titleInput = screen.getByPlaceholderText('タイトル🐾');
-    fireEvent.change(titleInput, {
-      target: {
-        value: 'タイトル'
-      }
+  describe('新規投稿できる', () => {
+    beforeEach(() => {
+      useSelectorMock.mockImplementation((selector: any) => selector(mockStore));
     });
-    expect(screen.getByText('タイトル')).toBeInTheDocument();
-  });
 
-  it('サブタイトル入力できる', async () => {
-    renderComponent();
-
-    const titleInput = screen.getByPlaceholderText('サブタイトル🐾');
-    fireEvent.change(titleInput, {
-      target: {
-        value: 'サブタイトル'
-      }
+    afterEach(() => {
+      jest.clearAllMocks();
     });
-    expect(screen.getByText('サブタイトル')).toBeInTheDocument();
-  });
 
-  it('説明入力できる', async () => {
-    renderComponent();
+    const mockStore = {
+      post: {
+        loading: false,
+      },
+      category: {
+        categories: [
+          { id: '1', name: 'TEST_CATEGORY' },
+        ],
+      },
+    };
 
-    const titleInput = screen.getByPlaceholderText('説明🐾');
-    fireEvent.change(titleInput, {
-      target: {
-        value: '説明'
-      }
+    const renderComponent = () => render(
+      <Provider store={store}>
+        <PostEdit />
+      </Provider>
+    );
+
+    it('タイトル入力できる', async () => {
+      renderComponent();
+
+      const titleInput = screen.getByPlaceholderText('タイトル🐾');
+      fireEvent.change(titleInput, {
+        target: {
+          value: 'タイトル'
+        }
+      });
+      expect(screen.getByText('タイトル')).toBeInTheDocument();
     });
-    expect(screen.getByText('説明')).toBeInTheDocument();
-  });
 
-  it('カテゴリ選択できる', async () => {
-    renderComponent();
+    it('サブタイトル入力できる', async () => {
+      renderComponent();
 
-    fireEvent.change(screen.getAllByRole('button')[1], {
-      target: {
-        value: '1'
-      }
+      const titleInput = screen.getByPlaceholderText('サブタイトル🐾');
+      fireEvent.change(titleInput, {
+        target: {
+          value: 'サブタイトル'
+        }
+      });
+      expect(screen.getByText('サブタイトル')).toBeInTheDocument();
     });
-    fireEvent.click(screen.getByText('category'));
-    expect(screen.getByText('category')).toBeInTheDocument();
+
+    it('説明入力できる', async () => {
+      renderComponent();
+
+      const titleInput = screen.getByPlaceholderText('説明🐾');
+      fireEvent.change(titleInput, {
+        target: {
+          value: '説明'
+        }
+      });
+      expect(screen.getByText('説明')).toBeInTheDocument();
+    });
+
+    it('カテゴリ選択できる', async () => {
+      renderComponent();
+
+      userEvent.click(screen.getAllByRole('button')[1]);
+      userEvent.click(screen.getByText('TEST_CATEGORY'));
+      expect(screen.getAllByText('TEST_CATEGORY')[0]).toBeInTheDocument();
+    });
+
+    it('「投稿」が表示されている', () => {
+      renderComponent();
+
+      expect(screen.getByText('投稿！')).toBeInTheDocument();
+    });
   });
 
-  it('「投稿」が表示されている', () => {
-    renderComponent();
+  describe('編集できる', () => {
+    const axiosMock = axios as jest.Mocked<typeof axios>;
+    const renderComponent = () => render(
+      <Provider store={store}>
+        <PostEdit />
+      </Provider>
+    );
 
-    expect(screen.getByText('投稿！')).toBeInTheDocument();
+    beforeEach(() => {
+      // @ts-ignore
+      delete window.location;
+      // @ts-ignore
+      window.location = {
+        pathname: 'http://dummy.com/post/edit/1',
+      };
+      useSelectorMock.mockImplementation((selector: any) => selector(mockStore));
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    const mockStore = {
+      post: {
+        loading: false,
+      },
+      category: {
+        categories: [
+          { id: '1', name: 'UPDATE_CATEGORY' },
+        ],
+      },
+    };
+    const post: PostType = {
+      id: '1',
+      title: 'TEST_Title',
+      subTitle: 'SubTitle',
+      body: 'Boyd',
+      images: [],
+      categoryId: '1',
+      cuteCount: 0,
+      favCount: 0,
+      goodCount: 0,
+      coolCount: 0,
+    };
+
+    it('タイトル再入力できる', async () => {
+      axiosMock.get.mockImplementationOnce(() => Promise.resolve({ data: { post } }));
+      renderComponent();
+
+      const titleInput = screen.getByText('TEST_Title');
+      fireEvent.change(titleInput, {
+        target: {
+          value: 'UPDATE_Title'
+        }
+      });
+
+      expect(screen.getByText('UPDATE_Title')).toBeInTheDocument();
+    });
+
+    it('サブタイトル再入力できる', async () => {
+      renderComponent();
+
+      const titleInput = screen.getByPlaceholderText('サブタイトル🐾');
+      fireEvent.change(titleInput, {
+        target: {
+          value: 'サブタイトル'
+        }
+      });
+      expect(screen.getByText('サブタイトル')).toBeInTheDocument();
+    });
+
+    it('説明再入力できる', async () => {
+      renderComponent();
+
+      const titleInput = screen.getByPlaceholderText('説明🐾');
+      fireEvent.change(titleInput, {
+        target: {
+          value: '説明'
+        }
+      });
+      expect(screen.getByText('説明')).toBeInTheDocument();
+    });
+
+    it('カテゴリ選択できる', async () => {
+      renderComponent();
+
+      userEvent.click(screen.getAllByRole('button')[1]);
+      userEvent.click(screen.getByText('TEST_CATEGORY'));
+      expect(screen.getAllByText('TEST_CATEGORY')[0]).toBeInTheDocument();
+    });
+
+    it('「投稿」が表示されている', () => {
+      renderComponent();
+
+      expect(screen.getByText('投稿！')).toBeInTheDocument();
+    });
+
   });
 
 });
